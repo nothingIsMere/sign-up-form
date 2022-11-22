472b523eeee791b5237e1b954efe05d66526c633
Joel <joel.knoll1@gmail.com>
2022-11-21 23:44:17 -0500

    partial functionality to validate that password matches password confirmation

---
+++ script.js
@@ -0,0 +1,9 @@
+let pwd = document.getElementById("pwd");
+let pwdConfirm = document.getElementById("pwd-confirm");
+let createAcctBtn = document.getElementById("create-acct");
+
+createAcctBtn.addEventListener("click", () => {
+  if(pwd.value != pwdConfirm.value) {
+    alert("Passwords do not match");
+  }
+})
